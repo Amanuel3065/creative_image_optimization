15896d2637c4f1b6059e76699d7c4aee2a0e832c
Amanuel3065 <amanuelzewdu21@gmail.com>
2022-11-15 04:12:09 +0300

    modify dashboard

--- dashboard/applications/model.py
+++ dashboard/applications/model.py
@@ -21,26 +21,48 @@ def app():
     st.header("To calculate KPI, enter values below.")
 
     st.header("number of objects")
-    all_objects_count = st.number_input('all objects', key='a')
-    no_of_unique_objects = st.number_input('unique objects', key='b')
+    form = st.form(key="w")
+    allc, uqec = st.columns(3)
+    with allc:
+        all_objects_count = st.number_input('all objects', key='a')
+    with uqec:
+        no_of_unique_objects = st.number_input('unique objects', key='b')
+   
     
-    st.header("engagement button size in pixels")
-    eng_width = st.number_input('width of engagement button', key='c')
-    eng_height = st.number_input('height of engagement button', key='d')
+    
+
+
+
+    st.header("engagement button properties")
+
+    form = st.form(key="x")
+    engWc, engHc = st.columns(3)
+    with engWc:
+        eng_width = st.number_input('width of engagement button', key='c')
+    with engHc:
+        eng_height = st.number_input('height of engagement button', key='d')
+        
+    
+    
 
     st.header("LAR")
     LAR = st.number_input('logo proportion', key='e')
 
+
+   
     st.header("CTA properties")
     cta_text_word_count = st.number_input('word count', key='f')
-    cta_width = st.number_input('width', key='g')
-    cta_height = st.number_input('height', key='h')
-
-    redc, greenc, bluec = st.columns(3)
-    
+   
+    form = st.form(key="y")
+    ctaWc, ctaHc = st.columns(3)
+    with ctaWc:
+        cta_width = st.number_input('width', key='g')
+    with ctaHc:
+        cta_height = st.number_input('height', key='h')
+        
 
     form = st.form(key="z")
-    redc, greenc, bluec = st.columns(4)
+    redc, greenc, bluec = st.columns(3)
     with redc:
         red = form.number_input('red', key='i')
     with greenc:
